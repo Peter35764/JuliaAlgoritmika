2c71dbb1a215ae20ace0083c3907ee1ea7c16080
Peter35764 <90093462+Peter35764@users.noreply.github.com>
2022-06-15 18:53:07 +0300

    Update q4.jl

--- q4.jl
+++ q4.jl
@@ -1,17 +1,17 @@
-
 function simpleCountingSort(massive::AbstractArray) 
     acc = zeros(Int64, length(massive))
     for i in 1:length(massive)
-        acc[massive[i]] = acc[massive[i]] + 1;     
+        acc[massive[i]] = acc[massive[i]] + 1;
     end
     pos = 1;
-    for number in 1:length(massive)
-        for i in 1:acc[number] - 1
+    for number in 1:length(acc)
+        for i in 1:acc[number]
             massive[pos] = number;
             pos = pos + 1;
         end
     end
 end
+# Нерабочий код: (хз вообще зачем он нужен, whatever)
 #=
 function countSort(massive::AbstractArray) # от 0 до 100
     acc = zeros(Int64, length(massive))
